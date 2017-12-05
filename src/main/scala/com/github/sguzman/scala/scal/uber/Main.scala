e3443eb68b4ac92d2bcc3556765f7deba9639228
Salvador Guzman <guzmansalv@gmail.com>
2017-12-05 12:18:56 -0800

    simple get stub method

--- src/main/scala/com/github/sguzman/scala/scal/uber/Main.scala
+++ src/main/scala/com/github/sguzman/scala/scal/uber/Main.scala
@@ -1,7 +1,24 @@
 package com.github.sguzman.scala.scal.uber
 
+import fr.hmil.roshttp.HttpRequest
+import fr.hmil.roshttp.Protocol.HTTPS
+import monix.execution.Scheduler.Implicits.global
+
+import scala.concurrent.Await
+import scala.concurrent.duration.Duration
+
 object Main {
   def main(args: Array[String]): Unit = {
-    println("Hello, world!")
+    val cookie = args.head
+
+    val request = HttpRequest()
+      .withProtocol(HTTPS)
+      .withHost("partners.uber.com")
+      .withPath("/p3/platform_chrome_nav_data")
+
+    val responseFuture = request.send
+    val response = Await.result(responseFuture, Duration.Inf)
+    println(response.statusCode)
+    println(response.body)
   }
 }
