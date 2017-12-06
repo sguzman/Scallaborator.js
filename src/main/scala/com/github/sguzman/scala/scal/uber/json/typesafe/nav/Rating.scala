be08a8d4e5d4b205314e86cc5e7552d5fdfc48ea
Salvador Guzman <guzmansalv@gmail.com>
2017-12-05 20:50:01 -0800

    use hashmap instead of map

--- src/main/scala/com/github/sguzman/scala/scal/uber/json/typesafe/nav/Rating.scala
+++ src/main/scala/com/github/sguzman/scala/scal/uber/json/typesafe/nav/Rating.scala
@@ -1,8 +1,10 @@
 package com.github.sguzman.scala.scal.uber.json.typesafe.nav
 
+import scala.collection.immutable.HashMap
+
 case class Rating(
                  subject: Subject,
                  aggregateType: String,
                  displayValue: String,
-                 histogram: Map[String, Int]
+                 histogram: HashMap[String, Int]
                  )
