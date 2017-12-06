600752d8514cc3f515e99432e87e9a8e3897d43d
Salvador Guzman <guzmansalv@gmail.com>
2017-12-06 01:38:00 -0800

    moved login facilities to login object

---
+++ src/main/scala/com/github/sguzman/scala/scal/uber/Login.scala
@@ -0,0 +1,81 @@
+package com.github.sguzman.scala.scal.uber
+
+import com.github.sguzman.scala.scal.uber.json.typesafe.login.email.input.{Answer, Email, UserIdentifier}
+import com.github.sguzman.scala.scal.uber.json.typesafe.login.email.output.EmailOutput
+import com.github.sguzman.scala.scal.uber.json.typesafe.login.password.input
+import com.github.sguzman.scala.scal.uber.json.typesafe.login.password.input.Password
+import com.github.sguzman.scala.scal.uber.Util.requestUntilSuccess
+import io.circe.parser.parse
+import io.circe.parser.decode
+import io.circe.generic.auto._
+import io.circe.syntax._
+
+import scalaj.http.{Http, HttpResponse}
+
+object Login {
+  def loggedIn(cookie: String): Boolean =
+    parse(
+      requestUntilSuccess(
+        Http("https://partners.uber.com/p3/platform_chrome_nav_data")
+          .header("Cookie", cookie)).body
+    ) match {
+      case Left(_) => false
+      case Right(_) => true
+    }
+
+  def logMeIn(initCookie: String, email: String, pass: String) = {
+    val response = getLoginPage(initCookie)
+
+    val emailResponse = postEmail(response, email)
+    val _ = decode[EmailOutput](emailResponse.body)
+
+    val password = postPassword(emailResponse, pass)
+    val _ = decode[Password](password.body)
+    //TODO - implement login features
+    initCookie
+  }
+
+  def getLoginPage(cookie: String): HttpResponse[String] = {
+    val url = "https://auth.uber.com/login/?next_url=https%3A%2F%2Fpartners.uber.com"
+    val request = Http(url).header("Cookie", cookie)
+
+    val response = requestUntilSuccess(request)
+    response
+  }
+
+  def postEmail(response: HttpResponse[String], email: String): HttpResponse[String] = {
+    val cookie = response.cookies.mkString("; ")
+    val crsfToken = response.header("x-csrf-token").get
+
+    val emailObj = Email(Answer("VERIFY_INPUT_EMAIL", UserIdentifier(email)), init = true)
+    val emailPayload = emailObj.asJson.toString
+
+    val postUrl = "https://auth.uber.com/login/handleanswer"
+    val emailRequest = Http(postUrl)
+      .header("Cookie", cookie)
+      .header("x-csrf-token", crsfToken)
+      .header("Content-Type", "application/json")
+      .postData(emailPayload)
+
+    val emailResponse = requestUntilSuccess(emailRequest)
+    emailResponse
+  }
+
+  def postPassword(response: HttpResponse[String], pass: String): HttpResponse[String] = {
+    val cookie = response.cookies.mkString("; ")
+    val crsfToken = response.header("x-csrf-token").get
+
+    val passObj = Password(input.Answer("VERIFY_PASSWORD", pass), rememberMe = true)
+    val body = passObj.asJson.toString
+
+    val postUrl = "https://auth.uber.com/login/handleanswer"
+    val passRequest = Http(postUrl)
+      .header("Cookie", cookie)
+      .header("x-csrf-token", crsfToken)
+      .header("Content-Type", "application/json")
+      .postData(body)
+
+    val passResponse = requestUntilSuccess(passRequest)
+    passResponse
+  }
+}
