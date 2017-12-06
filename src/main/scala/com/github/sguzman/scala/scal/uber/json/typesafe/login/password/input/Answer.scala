17c48d659503bb554a25080585dbcdccf9df4fe6
Salvador Guzman <guzmansalv@gmail.com>
2017-12-06 01:32:57 -0800

    check type safety of password response payload

---
+++ src/main/scala/com/github/sguzman/scala/scal/uber/json/typesafe/login/password/input/Answer.scala
@@ -0,0 +1,6 @@
+package com.github.sguzman.scala.scal.uber.json.typesafe.login.password.input
+
+case class Answer(
+                 `type`: String,
+                 password: String
+                 )
---
+++ src/main/scala/com/github/sguzman/scala/scal/uber/json/typesafe/login/password/input/Password.scala
@@ -0,0 +1,6 @@
+package com.github.sguzman.scala.scal.uber.json.typesafe.login.password.input
+
+case class Password(
+                   answer: Answer,
+                   rememberMe: Boolean
+                   )
