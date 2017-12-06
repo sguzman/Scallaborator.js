c9a8cc6cce5d55f334a96f14635cc457d28f9e67
Salvador Guzman <guzmansalv@gmail.com>
2017-12-05 22:54:12 -0800

    simple stub solution to print true or false if logged in

--- src/main/scala/com/github/sguzman/scala/scal/uber/Main.scala
+++ src/main/scala/com/github/sguzman/scala/scal/uber/Main.scala
@@ -1,18 +1,32 @@
 package com.github.sguzman.scala.scal.uber
 
-import io.circe.parser._
+import java.net.SocketTimeoutException
 
-import scalaj.http.{Http, HttpResponse}
+import io.circe.parser.parse
+
+import scala.util.{Failure, Success}
+import scalaj.http.Http
 
 object Main {
   def main(args: Array[String]): Unit = {
     val initCookie = args.head
     val url = "https://partners.uber.com/p3/platform_chrome_nav_data"
-    val logged = get(url, initCookie)
-    val json = parse(logged.body)
-    println(json)
+    val logged = loggedIn(initCookie)
+    println(logged)
   }
 
-  def get(url: String, cookie: String): HttpResponse[String] =
-    Http(url).header("Cookie", cookie).asString
+  def body(url: String, cookie: String): String =
+    util.Try(Http(url).header("Cookie", cookie).asString) match {
+      case Success(v) => v.body
+      case Failure(e) => e match {
+        case _: SocketTimeoutException => body(url, cookie)
+      }
+    }
+
+  def loggedIn(cookie: String): Boolean =
+    parse(body("https://partners.uber.com/p3/platform_chrome_nav_data", cookie)
+    ) match {
+      case Left(_) => false
+      case Right(_) => true
+    }
 }
