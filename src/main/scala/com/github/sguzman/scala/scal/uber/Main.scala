1273cbf26a5de651186bb9ee6d91c1eb0c2fd0ab
Salvador Guzman <guzmansalv@gmail.com>
2017-12-05 21:07:27 -0800

    stub login logic

--- src/main/scala/com/github/sguzman/scala/scal/uber/Main.scala
+++ src/main/scala/com/github/sguzman/scala/scal/uber/Main.scala
@@ -3,18 +3,37 @@ package com.github.sguzman.scala.scal.uber
 import com.github.sguzman.scala.scal.uber.json.typesafe.nav.Item
 import com.google.gson.GsonBuilder
 
-import scalaj.http.Http
+import scala.util.{Failure, Success}
+import scalaj.http.{Http, HttpResponse}
 
 object Main {
   def main(args: Array[String]): Unit = {
-    val cookie = args.head
+    val initCookie = args.head
+    val logged = loggedIn(initCookie)
 
-    val request = Http("https://partners.uber.com/p3/platform_chrome_nav_data")
-    val response = request.header("Cookie", cookie).asString
+    val cookie = if (logged) initCookie
+    else logMeIn(cookie)
 
-    val gson = new GsonBuilder().create
 
-    val json = gson.fromJson(response.body, classOf[Item])
-    println(json)
+    println(logged)
+  }
+
+  def get(url: String, cookie: String): HttpResponse[String] =
+    util.Try(Http(url).header("Cookie", cookie).asString) match {
+      case Success(v) => v
+      case Failure(e) => get(url, cookie)
+    }
+
+  def loggedIn(cookie: String): Boolean =
+    util.Try(
+      new GsonBuilder().create.fromJson(
+        get("https://partners.uber.com/p3/platform_chrome_nav_data", cookie).body
+      , classOf[Item])) match {
+      case Success(_) => true
+      case Failure(_) => false
+    }
+
+  def logMeIn(firstCookie: String): String = {
+    ""
   }
 }
