09b567b6e22929960a98be6145a5cfd6c5134383
Salvador Guzman <guzmansalv@gmail.com>
2017-12-05 21:34:54 -0800

    simple case that checks for logged in cookie

--- src/main/scala/com/github/sguzman/scala/scal/uber/Main.scala
+++ src/main/scala/com/github/sguzman/scala/scal/uber/Main.scala
@@ -1,5 +1,6 @@
 package com.github.sguzman.scala.scal.uber
 
+import com.github.sguzman.scala.scal.uber.json.typesafe.login.email.input.{Answer, Email, UserIdentifier}
 import com.github.sguzman.scala.scal.uber.json.typesafe.nav.Item
 import com.google.gson.GsonBuilder
 
@@ -10,19 +11,17 @@ object Main {
   def main(args: Array[String]): Unit = {
     val initCookie = args.head
     val logged = loggedIn(initCookie)
+    val email = args(1)
+    val pass = args(2)
 
     val cookie = if (logged) initCookie
-    else logMeIn(cookie)
+    else logMeIn(initCookie, email, pass)
 
-
-    println(logged)
+    println(cookie)
   }
 
   def get(url: String, cookie: String): HttpResponse[String] =
-    util.Try(Http(url).header("Cookie", cookie).asString) match {
-      case Success(v) => v
-      case Failure(e) => get(url, cookie)
-    }
+    Http(url).header("Cookie", cookie).asString
 
   def loggedIn(cookie: String): Boolean =
     util.Try(
@@ -33,7 +32,16 @@ object Main {
       case Failure(_) => false
     }
 
-  def logMeIn(firstCookie: String): String = {
-    ""
+  def logMeIn(firstCookie: String, email: String, pass: String): String = {
+    val bodyObj =
+      Email(Answer("VERIFY_INPUT_EMAIL", UserIdentifier(email)), init = true)
+
+    val gson = new GsonBuilder().create
+    val body = gson.toJson(bodyObj)
+    println(body)
+
+    val url = "https://auth.uber.com/login/handleanswer"
+    val response = Http(url).postData(body).asString
+    response.body
   }
 }
