d8fa43c3d148a3e325a0bd39f7eb3820c2cb7ce3
Salvador Guzman <guzmansalv@gmail.com>
2017-12-06 18:43:14 -0800

    added json type safety classes

---
+++ src/main/scala/com/github/sguzman/scala/scal/uber/safetyfirst/Tweet.scala
@@ -0,0 +1,7 @@
+package com.github.sguzman.scala.scal.uber.safetyfirst
+
+case class Tweet(
+                created_at: String,
+                id: Long,
+                text: String
+                )
---
+++ src/main/scala/com/github/sguzman/scala/scal/uber/safetyfirst/TweetTopLevel.scala
@@ -0,0 +1,5 @@
+package com.github.sguzman.scala.scal.uber.safetyfirst
+
+case class TweetTopLevel(
+                        tweets: Array[Tweet]
+                        )
