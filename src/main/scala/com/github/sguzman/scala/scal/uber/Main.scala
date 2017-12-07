c4937792311674f5de32ca24368e1bb1f12454f1
Salvador Guzman <guzmansalv@gmail.com>
2017-12-06 18:12:04 -0800

    append hello world text node to page

--- src/main/scala/com/github/sguzman/scala/scal/uber/Main.scala
+++ src/main/scala/com/github/sguzman/scala/scal/uber/Main.scala
@@ -1,7 +1,17 @@
 package com.github.sguzman.scala.scal.uber
 
+import org.scalajs.dom
+import org.scalajs.dom.document
+
 object Main {
   def main(args: Array[String]): Unit = {
-    println("Hello world")
+    appendPar(document.body, "Hello world")
+  }
+
+  def appendPar(targetNode: dom.Node, text: String): Unit = {
+    val parNode = document.createElement("p")
+    val textNode = document.createTextNode(text)
+    parNode.appendChild(textNode)
+    targetNode.appendChild(parNode)
   }
 }
