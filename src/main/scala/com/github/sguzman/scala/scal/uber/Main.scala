0a417b6579e02c15d3efdcaab79335b9fb7a791d
Salvador Guzman <guzmansalv@gmail.com>
2017-12-07 10:05:47 -0800

    append tweets to html

--- src/main/scala/com/github/sguzman/scala/scal/uber/Main.scala
+++ src/main/scala/com/github/sguzman/scala/scal/uber/Main.scala
@@ -1,13 +1,42 @@
 package com.github.sguzman.scala.scal.uber
 
+import com.github.sguzman.scala.scal.uber.safetyfirst.TweetTopLevel
+import fr.hmil.roshttp.HttpRequest
+import fr.hmil.roshttp.response.SimpleHttpResponse
 import google.maps.LatLng
+import io.circe.generic.auto._
+import io.circe.parser.decode
+import monix.execution.Scheduler.Implicits.global
 import org.scalajs.dom.document
 
 import scala.scalajs.js
+import scala.util.{Failure, Success}
+import scalatags.JsDom.all._
 
 object Main {
+  var gmap: Option[google.maps.Map] = None
   def main(args: Array[String]): Unit = {
     google.maps.event.addDomListener(org.scalajs.dom.window, "load", initialize())
+
+    val url = "https://tranquil-island-19340.herokuapp.com/"
+    val request = HttpRequest(url)
+    request.send.onComplete({
+      case res: Success[SimpleHttpResponse] =>
+        val body = res.get.body
+        val tweets = decode[TweetTopLevel](body).right.get.tweets
+        val tweetAnchor = document.getElementById("tweets")
+        tweets foreach (t => {
+          tweetAnchor.appendChild(
+            div(
+              h1(t.id),
+              h3(t.created_at),
+              p(t.text)
+            ).render)
+        })
+      case error: Failure[SimpleHttpResponse] => Console.err.println(error)
+    })
+
+
   }
 
   def initialize() = js.Function {
@@ -18,7 +47,7 @@ object Main {
       streetViewControl = false,
       mapTypeControl = false
     )
-    val gmap = new google.maps.Map(document.getElementById("map"), opts)
+    this.gmap = Some(new google.maps.Map(document.getElementById("map"), opts))
     ""
   }
 }
