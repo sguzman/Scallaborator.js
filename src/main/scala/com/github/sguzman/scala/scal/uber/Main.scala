79fd33775f9aa42b6f86fc553e4d714e314f62fe
Salvador Guzman <guzmansalv@gmail.com>
2017-12-06 17:59:45 -0800

    hello world

--- src/main/scala/com/github/sguzman/scala/scal/uber/Main.scala
+++ src/main/scala/com/github/sguzman/scala/scal/uber/Main.scala
@@ -1,18 +1,7 @@
 package com.github.sguzman.scala.scal.uber
 
-import java.util.UUID
-
-import com.github.sguzman.scala.scal.uber.args.{Config, Parser}
-import com.github.sguzman.scala.scal.uber.json.typesafe.data.all_data.AllDataStatement
-import com.github.sguzman.scala.scal.uber.json.typesafe.data.statement.Statement
-import com.github.sguzman.scala.scal.uber.json.typesafe.data.trip.Trip
-import io.circe.generic.auto._
-import io.circe.parser.decode
-
-import scala.util.{Failure, Success}
-
 object Main {
   def main(args: Array[String]): Unit = {
-
+    println("Hello world")
   }
 }
