b7b746cd0ce1f0d157099022d57c2aa11ebb5e56
Salvador Guzman <guzmansalv@gmail.com>
2017-12-06 17:58:20 -0800

    gut out main method

--- src/main/scala/com/github/sguzman/scala/scal/uber/Main.scala
+++ src/main/scala/com/github/sguzman/scala/scal/uber/Main.scala
@@ -13,63 +13,6 @@ import scala.util.{Failure, Success}
 
 object Main {
   def main(args: Array[String]): Unit = {
-    val config = new Parser().parse(args, Config()) match {
-      case Some(c) => c
-      case None =>
-        System.exit(1)
-        Config()
-    }
 
-    val allData = Util.requestUntilSuccess(
-      Util.getRequest("https://partners.uber.com/p3/money/statements/all_data/", config.cookie)
-    )
-
-    val str = allData.body
-    val allDataStatements = decode[Array[AllDataStatement]](str).right.get
-    val statementUUIDs = allDataStatements map (_.uuid)
-
-    val partialToUUID = toTripUUID(config.cookie, _: UUID)
-    val tripUUIDS = statementUUIDs.flatMap(partialToUUID)
-    println(tripUUIDS.length)
-
-    val partialToTrip = trip(config.cookie, _: UUID)
-    val grabTrip = tripUUIDS.map(partialToTrip)
-    println(grabTrip)
-  }
-
-  def toTripUUID(cookie: String, statementUUID: UUID): List[UUID] = {
-    val url = s"https://partners.uber.com/p3/money/statements/view/$statementUUID"
-    val request = Util.getRequest(url, cookie)
-    val response = Util.requestUntilSuccessIndef(request, (t, i) => {
-      util.Try(decode[Statement](t.body).right.get.body.driver.trip_earnings.trips.keySet.toList) match {
-        case Success(_) =>
-          println(s"!!!$statementUUID!!! @ $i")
-          true
-        case Failure(_) => false
-      }
-    })
-
-    val obj = decode[Statement](response)
-    val right = obj.right
-    val getRight = right.get
-    val uuids = getRight.body.driver.trip_earnings.trips.keySet.toList
-    uuids
-  }
-
-  def trip(cookie: String, tripUUID: UUID): Trip = {
-    val url = s"https://partners.uber.com/p3/money/trips/trip_data/$tripUUID"
-    val request = Util.getRequest(url, cookie)
-    val response = Util.requestUntilSuccessIndef(request, (t, i) => {
-      util.Try(decode[Trip](t.body).right.get) match {
-        case Success(_) =>
-          println(s"!!!$tripUUID!!! @ $i")
-          true
-        case Failure(_) => false
-      }
-    })
-
-    val body = response
-    val obj = decode[Trip](body).right.get
-    obj
   }
 }
