81ebf9bcc67deb3288f245fbdee00bcc03e05ef3
Salvador Guzman <guzmansalv@gmail.com>
2017-12-07 16:37:37 -0800

    print out latlng of tweet

--- src/main/scala/com/github/sguzman/scala/scal/uber/Main.scala
+++ src/main/scala/com/github/sguzman/scala/scal/uber/Main.scala
@@ -1,8 +1,7 @@
 package com.github.sguzman.scala.scal.uber
 
-import com.github.sguzman.scala.scal.uber.safetyfirst.TweetTopLevel
+import com.github.sguzman.scala.scal.uber.safetyfirst.{Tweet, TweetTopLevel}
 import fr.hmil.roshttp.HttpRequest
-import fr.hmil.roshttp.response.SimpleHttpResponse
 import google.maps.LatLng
 import io.circe.generic.auto._
 import io.circe.parser.decode
@@ -11,7 +10,6 @@ import org.scalajs.dom.document
 
 import scala.scalajs.js
 import scala.util.{Failure, Success}
-import scalatags.JsDom.all._
 
 object Main {
   var gmap: Option[google.maps.Map] = None
@@ -20,23 +18,29 @@ object Main {
 
     val url = "https://tranquil-island-19340.herokuapp.com/"
     val request = HttpRequest(url)
-    request.send.onComplete({
-      case res: Success[SimpleHttpResponse] =>
-        val body = res.get.body
-        val tweets = decode[TweetTopLevel](body).right.get.tweets
-        val tweetAnchor = document.getElementById("tweets")
-        tweets foreach (t => {
-          tweetAnchor.appendChild(
-            div(
-              h1(t.id),
-              h3(t.created_at),
-              p(t.text)
-            ).render)
-        })
-      case error: Failure[SimpleHttpResponse] => Console.err.println(error)
+    request.send
+      .map(_.body)
+      .map(decode[TweetTopLevel])
+      .map(_.right)
+      .map(_.get)
+      .map(_.tweets)
+      .onComplete({
+        case tweets: Success[Array[Tweet]] =>
+          val latLngs = tweets
+            .value
+            .flatMap(_.text.split("\\*"))
+            .filter("[0-9] -[0-9]".r.findFirstIn(_).isDefined)
+            .map(_.split(" "))
+            .map(t => {
+              val (preA, postA) = t.head.splitAt(2)
+              val (preB, postB) = t(1).splitAt(4)
+              (preA ++ "." ++ postA, preB ++ "." ++ postB)
+            })
+            .map(t => (t._1.toDouble, t._2.toDouble))
+            .map(t => new google.maps.LatLng(t._1, t._2))
+          latLngs.toList foreach println
+        case error: Failure[Array[Tweet]] => Console.err.println(error)
     })
-
-
   }
 
   def initialize() = js.Function {
